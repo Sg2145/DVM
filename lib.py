1d7bea2f42a68ab62194fa0736a0a5a3536864fe
Sg2145 <117072632+Sg2145@users.noreply.github.com>
2022-12-13 23:24:44 +0530

    Add files via upload

--- lib.py
+++ lib.py
@@ -1,24 +1,51 @@
-global self
-
-
 class book():
     def __init__(self, Name, ISBN, Author):
-        self.Name = []
-        self.ISBN = []
-        self.Author = []
+        self.Name = Name
+        self.ISBN = ISBN
+        self.Author = Author
+
+    def __str__(self):
+        return f"{self.Name} {self.ISBN} {self.Author}"
+
+    def borrow_book(self, borrower):
+        if self.is_available():
+            self.borrow = borrower
+        else:
+            print('book is not available right now. ')
+
+    def return_book(self):
+        pass
 
 
 class Shelf():
-    def add_books(Name, ISBN, Author):
-        self.Name.append(Name)
-        self.ISBN.append(ISBN)
-        self.Author.append(Author)
+    def __init__(self, genre):
+        self.genre = genre
+        self.Books = []
+
+    def add_books(self, Name, ISBN, Author):
+        self.Books.append(book(Name, ISBN, Author))
 
     def show_catalog(self):
-        print("Name \t \t ISBN \t \t Author")
-        for i in range(n):
-            print(self.Name[i], "\t", "\t", self.ISBN[i],
-                  "\t", "\t", self.Author[i])
+        for i in self.Books:
+            print(i)
+        # print("Name \t \t ISBN \t \t Author")
+        # for i in range(n):
+        #     print(self.Name[i], "\t", "\t", self.ISBN[i],
+        #           "\t", "\t", self.Author[i])
+
+    def get_books_count(self, count):
+        self.count = count
+        return len(self.Books)
+
+    def remove_book(self, Name):
+        self.Books.remove(Name)
+
+    def populate_book(self, file_name):
+        workbook = xl.load_workbook(filename=file_name)
+        sheet = workbook.active
+        dim = sheet.calculate_dimension()
+        for row in sheet[dim]:
+            self.add_books(book(**tuple(row)))
 
 
 n = int(input("Enter the number of books to be added : "))
@@ -26,4 +53,7 @@ for i in range(n):
     Name = input("Book Name : ")
     ISBN = int(input("ISBN Number : "))
     Author = input("Author's Name : ")
-    Shelf.add_books(Name, ISBN, Author)
+    sh = Shelf(input("Enter the Genre :"))
+    sh.add_books(Name, ISBN, Author)
+
+sh.show_catalog()
